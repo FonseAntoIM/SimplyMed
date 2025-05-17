25b2ee27bda76a73435d7756b9305b7510e79fd2
FonseAntoIM <antofons.im@gmail.com>
2025-05-17 07:22:18 -0600

    Actualizacion de documentacion: Packages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/SimplyMed/',  // <- importante: mismo nombre del repo
   plugins: [react()],
   test: {
     environment: 'jsdom',
